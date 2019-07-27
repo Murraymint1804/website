c6066e78eb2b8e4e9e5493bf3bb5ddea920d578f
Adam Murray <adam.murray@liberty-it.co.uk>
2019-07-27 01:53:16 +0100

    Updates

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [infoOnHover, setInfoOnHover] = useState(false);
+
+
+  useEffect(() => {
+    return () => {
+    }
+  })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header">
+        <h1>Adam Murray</h1>
+        <div className="links">
+          <h4><a href="http://www.google.com">Interactive</a></h4>
+          <h4>Contact</h4>
+          <h4>Portfolio</h4>
+          <h4>About</h4>
+        </div>
+      </div>
+      <div className="header-border"></div>
+      <div className="body">
+        <div>
+          {infoOnHover ? 
+            <div className="learn-more">
+              <h5>Learn More!</h5>
+            </div> : ""}
+          <p onMouseEnter={() => setInfoOnHover(true)} onMouseLeave={() => setInfoOnHover(false)}>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          {infoOnHover}
+        </div>
+      </div>
     </div>
   );
 }
